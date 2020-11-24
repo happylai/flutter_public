19e76a9d508bc1bcd76c9c895664583575ac4f18
wangyangjun <wangyangjun@huice.com>
2020-11-24 11:01:13 +0800

    添加数据转换的插件

---
+++ zs_flutter_data/android/src/main/java/com/zsxj/zs_flutter_data/ZsFlutterDataPlugin.java
@@ -0,0 +1,41 @@
+package com.zsxj.zs_flutter_data;
+
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+/** ZsFlutterDataPlugin */
+public class ZsFlutterDataPlugin implements FlutterPlugin, MethodCallHandler {
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private MethodChannel channel;
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "zs_flutter_data");
+    channel.setMethodCallHandler(this);
+  }
+
+  @Override
+  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+    if (call.method.equals("GB2312")) {
+      String str = call.arguments();
+      result.success(str.getBytes("GB2312"));
+      return;
+    }
+
+    result.notImplemented();
+  }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
+  }
+}
---
+++ zs_flutter_data/example/android/app/src/main/java/com/zsxj/zs_flutter_data_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.zsxj.zs_flutter_data_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}

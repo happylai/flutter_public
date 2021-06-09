0f9dc286540330a8b851b0810283419d63f93fd3
wangyangjun <wangyangjun@huice.com>
2021-06-09 18:30:13 +0800

    优化添加案例

--- zs_permisson/example/lib/main.dart
+++ zs_permisson/example/lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   void initState() {
     super.initState();
@@ -22,26 +21,73 @@ class _MyAppState extends State<MyApp> {
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-
-  }
+  Future<void> initPlatformState() async {}
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: GestureDetector(
-          onTap: (){
-            ZsPermisson.openBluetooth();
-          },
-          child: Center(
-            child: Text('on test'),
-          ),
-        )
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Plugin example app'),
       ),
-    );
+      body: Column(
+        children: [
+          GestureDetector(
+            onTap: () async {
+              var res = await ZsPermisson.checkLocationPermission();
+              print("定位权限 $res");
+            },
+            child: item("checkLocationPermission"),
+          ),
+          GestureDetector(
+            onTap: ()async {
+              var res = await ZsPermisson.checkLocationState();
+              print("定位状态 $res");
+            },
+            child: item("checkLocationState"),
+          ),
+          GestureDetector(
+            onTap: () {
+              ZsPermisson.openLocationState();
+            },
+            child: item("openLocationState"),
+          ),
+          GestureDetector(
+            onTap: () {
+              ZsPermisson.openLocationPermission();
+            },
+            child: item("openLocationPermission"),
+          ),
+          GestureDetector(
+            onTap: () {
+              ZsPermisson.openBluetoothPmission();
+            },
+            child: item("openBluetoothPmission"),
+          ),
+          GestureDetector(
+            onTap: () {
+              ZsPermisson.openBluetooth();
+            },
+            child: item("openBluetooth"),
+          ),
+        ],
+      ),
+    ));
   }
+}
+
+Widget item(text) {
+  return Container(
+    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+    margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+    decoration: BoxDecoration(
+      border: Border.all(
+        color: Colors.black,
+        width: 2,
+      )
+    ),
+    child: Center(
+      child: Text(text ?? "-"),
+    ),
+  );
 }
